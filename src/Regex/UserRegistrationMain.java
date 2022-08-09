ce9e1e34054c30f3d234c8354eb6db74aa830c19
MohanRaj <mraj86698@gmail.com>
2022-08-09 22:32:19 +0530

    Added TO Display Message.

---
+++ src/Regex/UserRegistrationMain.java
@@ -0,0 +1,11 @@
+package Regex;
+
+public class UserRegistrationMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is an User Registration for using regex.");
+
+	}
+
+}
