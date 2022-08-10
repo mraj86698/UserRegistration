18ed6afcff0865a92fe1769220a1123eb3b2fb36
MohanRaj <mraj86698@gmail.com>
2022-08-10 14:37:27 +0530

    Added to Password Rule 3

--- src/Regex/UserRegistration.java
+++ src/Regex/UserRegistration.java
@@ -5,109 +5,119 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 	Scanner sc = new Scanner(System.in);
+
 	/**
-	 * Validation for first Name
-	 * First name starts with Cap and has minimum 3 characters
+	 * Validation for first Name First name starts with Cap and has minimum 3
+	 * characters
 	 */
 	public void firstName() {
-		System.out.print("Enter First Name : " );
+		System.out.print("Enter First Name : ");
 		String FirstName = Utility.inputString();
-		boolean answer = Pattern.matches("([A-Z]*[a-z]*){2,}",FirstName);
+		boolean answer = Pattern.matches("([A-Z]*[a-z]*){2,}", FirstName);
 		if (answer) {
 			System.out.println("First Name is Valid");
+		} else {
+			System.out.println("First Name is Invalid ");
 		}
-        else {
-        	System.out.println("First Name is Invalid ");
-        }
-	}
-	/**
-	 * Validation for LastName
-	 * Last name starts with Cap and has minimum 3 characters
-	 */
-	public void lastName() {
-		System.out.print("Enter Last Name : " );
-		String LastName = Utility.inputString();
-		boolean answer = Pattern.matches("([A-Z]*[a-z]*){2,}",LastName);
-		if (answer) {
-			System.out.println("Last Name is Valid");
-		}
-        else {
-        	System.out.println("Last Name is Invalid ");
-        }
 	}
 
 	/**
-	 * Validation for Email
-	 * E.g. abc.xyz@bl.co.in
+	 * Validation for LastName Last name starts with Cap and has minimum 3
+	 * characters
+	 */
+	public void lastName() {
+		System.out.print("Enter Last Name : ");
+		String LastName = Utility.inputString();
+		boolean answer = Pattern.matches("([A-Z]*[a-z]*){2,}", LastName);
+		if (answer) {
+			System.out.println("Last Name is Valid");
+		} else {
+			System.out.println("Last Name is Invalid ");
+		}
+	}
+
+	/**
+	 * Validation for Email E.g. abc.xyz@bl.co.in
 	 * Email has 3 mandatory parts (abc, bl & co)
 	 * 2 optional (xyz & in) with precise @ and . positions
 	 */
 	public void email() {
-		System.out.print("Enter Email Id : " );
+		System.out.print("Enter Email Id : ");
 		String Email = Utility.inputString();
-		boolean answer = Pattern.matches("[a-z]+([.][a-z]+)*@bl[.]co([.]in)*",Email);
+		boolean answer = Pattern.matches("[a-z]+([.][a-z]+)*@bl[.]co([.]in)*", Email);
 		if (answer) {
 			System.out.println("Email is Valid");
+		} else {
+			System.out.println("Email is Invalid ");
 		}
-        else {
-        	System.out.println("Email is Invalid ");
-        }
 	}
 
 	/**
-	 * Validation for Mobile Number
-	 * E.g. 91 9374930253
-	 * Country code Follow by space  and 10 digit number.
+	 * Validation for Mobile Number E.g. 91 9374930253
+	 * Country code Follow by space and 10 digit number.
 	 */
 
 	public void mobileNumber() {
-		System.out.print("Enter MobileNumber : " );
+		System.out.print("Enter MobileNumber : ");
 		String MobileNumber = sc.nextLine();
-		boolean answer = Pattern.matches("91\\s[6789]{1}[0-9]{9}",MobileNumber);
+		boolean answer = Pattern.matches("91\\s[6789]{1}[0-9]{9}", MobileNumber);
 		if (answer) {
 			System.out.println("Phone Number is Valid");
+		} else {
+			System.out.println("Phone Number is Invalid ");
 		}
-        else {
-        	System.out.println("Phone Number is Invalid ");
-        }
 	}
+
 	/**
 	 * Password Rule 1
 	 * Minimum 8 Character
 	 */
-	 public void password1() {
+	public void password1() {
 
-	        System.out.print("Enter password  :: " );
-	        String password = sc.nextLine();
-	        boolean answer = Pattern.matches("[a-zA-Z0-9]{8,}",password);
-	        if (answer) {
-	        	System.out.println("Password is Valid");
-	        }
-	        else {
-	        	System.out.println("Password is Not Valid");
-	        }
-	 }
+		System.out.print("Enter password  :: ");
+		String password = sc.nextLine();
+		boolean answer = Pattern.matches("[a-zA-Z0-9]{8,}", password);
+		if (answer) {
+			System.out.println("Password is Valid");
+		} else {
+			System.out.println("Password is Not Valid");
+		}
+	}
 
-	 /**
-		 * Password Rule 2
-		 * Minimum 8 Character
-		 * AtLewast 1 UpperCase
-		 */
-		 public void password2() {
+	/**
+	 * Password Rule 2
+	 * Minimum 8 Character
+	 * AtLeast 1 UpperCase
+	 */
+	public void password2() {
 
-		        System.out.print("Enter password  :: " );
-		        String password = sc.nextLine();
-		        boolean answer = Pattern.matches("[A-Z]+[a-z0-9]{8,}",password);
-		        if (answer) {
-		        	System.out.println("Password is Valid");
-		        }
-		        else {
-		        	System.out.println("Password is Not Valid");
-		        }
-		 }
+		System.out.print("Enter password  :: ");
+		String password = sc.nextLine();
+		boolean answer = Pattern.matches("[A-Z]+[a-z0-9]{7,}", password);
+		if (answer) {
+			System.out.println("Password is Valid");
+		} else {
+			System.out.println("Password is Not Valid");
+		}
+	}
 
+	/**
+	 * Password Rule 2
+	 * Minimum 8 Character
+	 * AtLeast 1 UpperCase
+	 * AtLeast 1 Numeric Number
+	 */
 
+	public void password3() {
 
-
+		System.out.print("Enter password  :: ");
+		String password = sc.nextLine();
+		boolean answer = Pattern.matches("[A-Z]+[a-z]{6,}+[0-9]", password);
+		if (answer) {
+			System.out.println("Password is Valid");
+		} else {
+			System.out.println("Password is Not Valid");
+		}
+	}
 
 }
