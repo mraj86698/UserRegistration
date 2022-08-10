ee203b7f89b920b4410332e149fe1f2634de22bf
MohanRaj <mraj86698@gmail.com>
2022-08-10 13:34:08 +0530

    Added Validation for FirstName using Regex

--- src/Regex/UserRegistrationMain.java
+++ src/Regex/UserRegistrationMain.java
@@ -3,8 +3,9 @@ package Regex;
 public class UserRegistrationMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("This is an User Registration for using regex.");
+
+		UserRegistration user=new UserRegistration();
+		user.firstName();
 
 	}
 
